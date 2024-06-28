5e7970cb3d5b612078ff5bb88a30ae2df29b9aed
Jhoneiro <125927561+JhoneiroLove@users.noreply.github.com>
2024-06-28 00:30:59 -0500

    librerias hashset importadas

--- src/main/java/com/upao/petsnature/domain/entity/Usuario.java
+++ src/main/java/com/upao/petsnature/domain/entity/Usuario.java
@@ -11,6 +11,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
